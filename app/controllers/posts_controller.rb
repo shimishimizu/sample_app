8faf76eec1488dcb78b482b2bf649e21be4b80f0
清水 雅人 <shimizumasato@appleshinja.local>
2018-11-25 14:22:17 +0900

    [finish]3-5

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+  def new
+  	# 空のモデルをビューへ渡す
+  	@post = Post.new
+  end
+
+  def create
+
+  	post = Post.new(post_params)
+
+  	post.save
+
+  	redirect_to '/top'
+  end
+  
+  private
+
+  def post_params
+  	params.require(:post).permit(:title, :body)
+  end
+end
