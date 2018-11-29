81cb7667948af6e284312a58067733d5672d16d7
清水 雅人 <shimizumasato@appleshinja.local>
2018-11-29 22:25:47 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6b9caf40bf96c2df6ff6c39fabce6a0d030aeeea4b16f359d755c35876b9db7171f0b8e4e6d3d32586a009bffd0c66391d75432083778498581829f9db7902d9'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_135933) do
+ActiveRecord::Schema.define(version: 2018_11_29_130253) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
