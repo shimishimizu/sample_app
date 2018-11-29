ff559979ecef025f0b53bd76f967fa4cdc2bdcc7
清水 雅人 <shimizumasato@appleshinja.local>
2018-11-29 15:37:30 +0900

    [finish]3-7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,28 @@ class PostsController < ApplicationController
 
   	post.save
 
-  	redirect_to '/top'
+  	redirect_to post_path(post.id)
   end
-  
-  private
 
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+
+   private
   def post_params
   	params.require(:post).permit(:title, :body)
   end
